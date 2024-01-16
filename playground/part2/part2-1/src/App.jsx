c5e6ed0ab5f550bd5768f66d66111be77db8db4c
vkahara <valtterikahara@gmail.com>
2024-01-17 00:44:58 +0200

    progress on part2a

---
+++ playground/part2/part2-1/src/App.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+const App = (props) => {
+  const { notes } = props
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
+
+
+
