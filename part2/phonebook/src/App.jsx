46be9f601a0f3a957ce61e465addc82c5206bc6d
vkahara <valtterikahara@gmail.com>
2024-01-18 11:21:22 +0200

    exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -11,8 +11,14 @@ const App = () => {
     const personObject = {
       name: newName,
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
+
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
+    
   }
 
   const handleNameChange = (event) => {
