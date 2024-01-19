6558383e8e178ed06740efdbcda386fa391a0287
vkahara <valtterikahara@gmail.com>
2024-01-19 23:24:47 +0200

    progress on part2e

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
