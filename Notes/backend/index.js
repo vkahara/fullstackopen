2a33bceee0d2911614c8e908bd8d40f0a1581cdc
vkahara <valtterikahara@gmail.com>
2024-01-23 11:52:35 +0200

    progress on part3c reading material

--- Notes/backend/index.js
+++ Notes/backend/index.js
@@ -1,8 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-
 const app = express()
-
+const Note = require('./models/note')
 app.use(express.static('dist'))
 app.use(cors())
 
@@ -40,12 +40,14 @@ let notes = [
 ]
 
 
-app.get('/', (req, res) => {
+app.get('/', (request, response) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 const generateId = () => {
@@ -58,35 +60,26 @@ const generateId = () => {
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({ 
       error: 'content missing' 
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
+  Note.findById(request.params.id).then(Note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
-
-  response.json(note)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -98,7 +91,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- Notes/backend/mongo.js
+++ Notes/backend/mongo.js
@@ -8,24 +8,42 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url = 
-`mongodb+srv://fullstack:${password}@cluster0.bhjqkyy.mongodb.net/?retryWrites=true&w=majority`
+`mongodb+srv://fullstack:${password}@cluster0.bhjqkyy.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
+
 
 const noteSchema = new mongoose.Schema({
     content: String,
     important: Boolean,
 })
 
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-    content: 'HTML is Easy',
-    important: true,
+    content: 'wwwwwwwwwwwwwwwwww wh',
+    important: false,
 })
 
 note.save().then(result => {
-    console.log('note saved!')
+    console.log('note saved!', result)
     mongoose.connection.close()
 })
+
+/*
+Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+*/
