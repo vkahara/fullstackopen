17c964e28fdef577e8b1f27f1c2fbb0799be7170
vkahara <valtterikahara@gmail.com>
2024-01-25 12:13:21 +0200

    exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,8 +1,13 @@
+// dummy take blogs do nothing just return 1
 const dummy = (blogs) => {
-  
   return 1
 }
 
+// return sum of likes from a list of blog posts
+const totalLikes = (blogPosts) => {
+  return blogPosts.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
