f7569e196f5c57d423ed3852aae3b0604d02c269
vkahara <valtterikahara@gmail.com>
2024-01-29 10:16:35 +0200

    exercise 4.11

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -17,13 +17,16 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes
   })
+
+  if (blog.likes === undefined) {
+    blog.likes = 0
+  }
 
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
