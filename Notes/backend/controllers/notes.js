da829f819ac5e7fd995e8a406accbecb812455fa
vkahara <valtterikahara@gmail.com>
2024-02-01 09:48:31 +0200

    part4d reading material

--- Notes/backend/controllers/notes.js
+++ Notes/backend/controllers/notes.js
@@ -1,6 +1,8 @@
 const notesRouter = require('express').Router()
+const decode = require('jsonwebtoken/decode')
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note
@@ -18,10 +20,21 @@ notesRouter.get('/:id', async (request, response) => {
 
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body
-
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
