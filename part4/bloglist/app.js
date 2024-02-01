3ce4e188dd2120540b834168712151e7358be08a
vkahara <valtterikahara@gmail.com>
2024-02-01 12:33:40 +0200

    exercise 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -21,5 +22,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
