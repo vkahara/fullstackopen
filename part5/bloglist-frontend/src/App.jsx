12a502e279390c03b34700f65c82f0214ad079dd
vkahara <valtterikahara@gmail.com>
2024-02-05 08:46:40 +0200

    exercise 5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -15,15 +15,40 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+
       setUser(user)
       setUsername('')
       setPassword('')
+    } catch (exception) {
+      console.log('error', exception)
+    }
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    try {
+      window.localStorage.removeItem('loggedBlogappUser')
+      setUser(null)
+
     } catch (exception) {
       console.log('error', exception)
     }
@@ -59,7 +84,10 @@ const App = () => {
   const showBlogs = () => (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in
+        <button onClick={ handleLogout }>logout</button>
+      </p>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
