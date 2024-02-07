822646506662425f99eb02576abdddf5ed44cb89
vkahara <valtterikahara@gmail.com>
2024-02-07 14:16:00 +0200

    exercise 5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -137,6 +137,10 @@ const App = () => {
     </Togglable>
   )
 
+  const sortBlogsByLikes = (blogs) => {
+    return blogs.slice().sort((a, b) => b.likes - a.likes);
+  }
+
   const showBlogs = () => (
     <div>
       <h2>blogs</h2>
@@ -145,7 +149,7 @@ const App = () => {
         <button onClick={ handleLogout }>logout</button>
       </p>
       {blogForm()}
-      {blogs.map(blog =>
+      {sortBlogsByLikes(blogs).map(blog =>
         <Blog key={blog.id} blog={blog} like={likeBlog}/>
       )}
     </div>
