7095be459836ad9a1585261a51143060cb63f596
vkahara <valtterikahara@gmail.com>
2024-02-11 10:25:36 +0200

    created test suite and added beforeeach

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -3,29 +3,34 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
 
-test('render title and author but not url and likes', () => {
-  const blog = {
-    title: 'test title',
-    author: 'test author',
-    url: 'https://example.com',
-    likes: 9999,
-    user: {
+describe('<Blog />', () => {
+  let container
+
+  beforeEach(() => {
+    const blog = {
+      title: 'test title',
+      author: 'test author',
+      url: 'https://example.com',
+      likes: 9999,
+      user: {
+        id: 1,
+        name: 'testuser'
+      }
+    }
+    const user = {
       id: 1,
       name: 'testuser'
     }
-  }
+    container = render(<Blog blog={blog} user={user} />).container
+  })
 
-  const user = {
-    id: 1,
-    name: 'testuser'
-  }
+  test('render title and author but not url and likes', () => {
+    expect(container.textContent).toContain('test title')
+    expect(container.textContent).toContain('test author')
 
-  const { container } = render(<Blog blog={blog} user={user} />)
-
-  expect(container.textContent).toContain(blog.title)
-  expect(container.textContent).toContain(blog.author)
-
-  const div = container.querySelector('.togglableContent')
-  expect(div).toHaveStyle('display: none')
+    const div = container.querySelector('.togglableContent')
+    expect(div).toHaveStyle('display: none')
+  })
 
 })
+
