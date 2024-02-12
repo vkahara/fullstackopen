f1aa24162bcb286ca7ec4f576efbb9e5c25aff02
vkahara <valtterikahara@gmail.com>
2024-02-12 09:33:24 +0200

    part5d reading

--- Notes/Notes/src/components/Footer.jsx
+++ Notes/Notes/src/components/Footer.jsx
@@ -1,15 +1,15 @@
 const Footer = () => {
-    const footerStyle = {
-      color: 'green',
-      fontStyle: 'italic',
-      fontSize: 16
-    }
-    return(
-      <div style = {footerStyle}>
-        <br />
-        <em>Note app, Department of käsien heiluttelu, University of not Harvard (turkuamk) 2024</em>
-      </div>
-    )
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
   }
+  return(
+    <div style = {footerStyle}>
+      <br />
+      <em>Note app, Department of käsien heiluttelu, University of not Harvard (turkuamk) 2024</em>
+    </div>
+  )
+}
 
-  export default Footer
+export default Footer
