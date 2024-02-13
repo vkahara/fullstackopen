1c9e3a7cadbc946a8cd191c3972b258216117bf1
vkahara <valtterikahara@gmail.com>
2024-02-13 10:28:36 +0200

    Notes when logged in tests

--- Notes/Notes/cypress/e2e/note_app.cy.js
+++ Notes/Notes/cypress/e2e/note_app.cy.js
@@ -21,4 +21,22 @@ describe('Note app', function() {
 
     cy.contains('Valtteri Kähärä logged in')
   })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('log in').click()
+      cy.get('input:first').type('vkahara')
+      cy.get('input:last').type('labra123')
+      cy.get('#login-button').click()
+    })
+
+
+    it('a new note can be created', function() {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+  })
+
 })
