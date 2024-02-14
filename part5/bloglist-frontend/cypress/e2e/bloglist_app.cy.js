7041d1a3eac825e0d3ea2bda51ecee4559d01682
vkahara <valtterikahara@gmail.com>
2024-02-14 12:30:17 +0200

    exercise 5.18

--- part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -1,10 +1,39 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Valtteri Kähärä',
+      username: 'vkahara',
+      password: 'labra123'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:5173')
   })
 
   it('Login form is shown', function() {
-    // ...
+    cy.contains('login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('vkahara')
+      cy.get('#password').type('labra123')
+      cy.get('#login-button').click()
+
+      cy.contains('Valtteri Kähärä logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('vkahara')
+      cy.get('#password').type('wrongpassword')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.contains('Valtteri Kähärä logged in').should('not.exist')
+    })
   })
 })
