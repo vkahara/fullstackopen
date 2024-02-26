9b73590d154d8a141aa1e6d0a02240023cc33d52
vkahara <valtterikahara@gmail.com>
2024-02-26 08:30:30 +0200

    exercise 6.3

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -20,10 +20,29 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  switch(action.type) {
+    case 'VOTE': {
+      const id = action.payload.id
+      const anecdoteToVote = state.find(n => n.id === id)
+      const changedAnecdote = { 
+        ...anecdoteToVote, 
+        votes: anecdoteToVote.votes + 1
+      }
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : changedAnecdote 
+      )
+     }
+    default:
+      return state
+  }
 
-  return state
+}
+
+export const addVote = (id) => {
+  return {
+    type: 'VOTE',
+    payload: { id }
+  }
 }
 
 export default reducer
