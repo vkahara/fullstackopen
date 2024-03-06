d38db2e08c4e044e5e4404ceeaf97fbd6e53a4bc
vkahara <valtterikahara@gmail.com>
2024-03-06 10:29:27 +0200

    appendNote in noteReducer

--- reading/part6/counter/src/reducers/noteReducer.js
+++ reading/part6/counter/src/reducers/noteReducer.js
@@ -1,21 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id: 1,
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2,
-  },
-]
+const initialState = []
 
 const generateId = () =>
   Number((Math.random() * 1000000).toFixed(0))
-
 
 const noteSlice = createSlice({
   name: 'notes',
@@ -42,9 +30,12 @@ const noteSlice = createSlice({
       return state.map(note =>
         note.id !== id ? note : changedNote 
       )     
+    },
+    appendNote(state, action) {
+      state.push(action.payload)
     }
   },
 })
 
-export const { createNote, toggleImportanceOf } = noteSlice.actions
+export const { createNote, toggleImportanceOf, appendNote } = noteSlice.actions
 export default noteSlice.reducer
