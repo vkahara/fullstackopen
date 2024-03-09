dc1e2a5be10b0a81ab2ada325e62af649059030d
vkahara <valtterikahara@gmail.com>
2024-03-09 10:19:17 +0200

    added reading material

--- reading/part6/counter/src/reducers/noteReducer.js
+++ reading/part6/counter/src/reducers/noteReducer.js
@@ -1,9 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
-
-const initialState = []
-
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
+import noteService from '../services/notes'
 
 const noteSlice = createSlice({
   name: 'notes',
@@ -36,4 +32,12 @@ const noteSlice = createSlice({
 })
 
 export const { createNote, toggleImportanceOf, appendNote, setNotes } = noteSlice.actions
+
+export const initializeNotes = () => {
+  return async dispatch => {
+    const notes = await noteService.getAll()
+    dispatch(setNotes(notes))
+  }
+}
+
 export default noteSlice.reducer
