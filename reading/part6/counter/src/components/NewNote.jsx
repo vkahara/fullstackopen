8963fa309c54478ed9af97cc736a100b694c6464
vkahara <valtterikahara@gmail.com>
2024-03-09 10:43:27 +0200

    asynchronous actions and redux thunk

--- reading/part6/counter/src/components/NewNote.jsx
+++ reading/part6/counter/src/components/NewNote.jsx
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux'
 import { createNote } from '../reducers/noteReducer'
 import noteService from '../services/notes'
+import { createNote } from '../reducers/noteReducer'
 
 const NewNote = () => {
   const dispatch = useDispatch()
@@ -9,8 +10,7 @@ const NewNote = () => {
     event.preventDefault()
     const content = event.target.note.value
     event.target.note.value = ''
-    const newNote = await noteService.createNew(content)
-    dispatch(createNote(newNote))
+    dispatch(createNote(content))
   }
 
   return (
