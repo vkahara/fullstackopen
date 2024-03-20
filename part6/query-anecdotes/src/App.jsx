d8fe3a623cfc045829810d257f86308537d2ef2d
vkahara <valtterikahara@gmail.com>
2024-03-20 09:40:47 +0200

    exercise 6.20

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -19,6 +19,10 @@ const App = () => {
     return <div>loading data...</div>
   }
 
+  if ( result.isError ) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
   const anecdotes = result.data
 
   return (
