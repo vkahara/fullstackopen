86ce154a19367f2db7aa1df26b863e25dfae5f8c
vkahara <valtterikahara@gmail.com>
2024-03-31 09:32:50 +0300

    exercise 7.8

--- part7/ultimate-hooks/src/App.jsx
+++ part7/ultimate-hooks/src/App.jsx
@@ -18,10 +18,20 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    setResources(response.data)
+  }
 
-  const create = (resource) => {
-    // ...
+  useEffect(() => {
+    getAll()
+  }, [])
+  
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    getAll()
+    return response.data
   }
 
   const service = {
