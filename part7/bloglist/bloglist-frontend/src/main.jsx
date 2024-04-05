f73dbd3ffb8502b5a838690472296b98ba3d9d83
vkahara <valtterikahara@gmail.com>
2024-04-05 09:08:01 +0300

    queryClient

--- part7/bloglist/bloglist-frontend/src/main.jsx
+++ part7/bloglist/bloglist-frontend/src/main.jsx
@@ -1,5 +1,13 @@
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import App from './App'
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+)
