b8c8017c5aca845b83944567faa2bfd7d265cc26
vkahara <valtterikahara@gmail.com>
2024-04-06 12:49:20 +0300

    exercise 7.14

--- part7/bloglist/bloglist-frontend/src/views/Users.jsx
+++ part7/bloglist/bloglist-frontend/src/views/Users.jsx
@@ -2,19 +2,33 @@ import userService from '../services/users'
 import { useQuery } from '@tanstack/react-query'
 
 const Users = () => {
-  const {
-    data: users,
-    isLoading,
-    error,
-  } = useQuery({
+  const { data: users, isLoading } = useQuery({
     queryKey: ['users'],
     queryFn: () => userService.getAll(),
   })
 
+  if (isLoading) return <div>loading...</div>
+
   return (
     <div>
       <h2>Users</h2>
-      {users && users.map(user => <li key={user.id}>{user.name}</li>)}
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>blogs created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users &&
+            users.map(user => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.blogs.length}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   )
 }
