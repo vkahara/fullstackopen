73d1194bd161a228bc9d7b9e9ca5126ca9a8941e
vkahara <valtterikahara@gmail.com>
2024-04-13 09:20:21 +0300

    exercise 8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -110,7 +110,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(name: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -119,7 +119,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.name) {
+        return books;
+      }
+      const author = authors.find((p) => p.name === args.name);
+      return books.filter((book) => book.author === author.name);
+    },
     allAuthors: () => authors
   },
   Author: {
