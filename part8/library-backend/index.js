e51e156472648799016f9dd1bca5de4b79cd154e
vkahara <valtterikahara@gmail.com>
2024-04-15 09:07:36 +0300

    exercise 8.7

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -123,6 +123,10 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `;
 
@@ -168,6 +172,15 @@ const resolvers = {
       }
       books = books.concat(book);
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((p) => p.name === args.name);
+      if (!author) {
+        return null;
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((p) => (p.name === args.name ? updatedAuthor : p));
+      return updatedAuthor;
     }
   }
 };
