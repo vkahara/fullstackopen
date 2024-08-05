e44b6b5a201f674f7ebd7a399e7f8a5a174a13cc
vkahara <valtterikahara@gmail.com>
2024-08-05 12:47:16 +0300

    part8b reading frontend

--- reading/part8/graphql-phonebook-frontend/src/App.jsx
+++ reading/part8/graphql-phonebook-frontend/src/App.jsx
@@ -1,27 +1,33 @@
-import { gql, useQuery } from '@apollo/client'
+import { useQuery } from '@apollo/client'
+import { useState } from 'react'
 
 import Persons from './components/Persons'
-
-const ALL_PERSONS = gql`
-  query {
-    allPersons  {
-      name
-      phone
-      id
-    }
-  }
-`
+import PersonForm from './components/PersonForm'
+import Notify from './components/Notify'
+import { ALL_PERSONS } from './queries'
+import PhoneForm from './components/PhoneForm'
 
 const App = () => {
   const result = useQuery(ALL_PERSONS)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   if (result.loading)  {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
     <div>
-      <Persons persons={result.data.allPersons}/>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons = {result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
     </div>
   )
 }
