5f7d5fb628b89fc406b4e22e6be759add69a40d7
vkahara <valtterikahara@gmail.com>
2024-09-02 09:30:57 +0300

    part8c reading validation

--- reading/part8/graphql-phonebook-backend/index.js
+++ reading/part8/graphql-phonebook-backend/index.js
@@ -1,12 +1,10 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
-const { v1: uuid } = require("uuid");
 const { GraphQLError } = require("graphql");
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 const Person = require("./models/person");
-
 require("dotenv").config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -16,11 +14,34 @@ console.log("connecting to", MONGODB_URI);
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
-    console.log("connected to mongodb");
+    console.log("connected to MongoDB");
   })
   .catch((error) => {
-    console.log("error connecting to mongodb", error.message);
+    console.log("error connection to MongoDB:", error.message);
   });
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    phone: "040-123543",
+    street: "Tapiolankatu 5 A",
+    city: "Espoo",
+    id: "3d594650-3436-11e9-bc57-8b80ba54c431"
+  },
+  {
+    name: "Matti Luukkainen",
+    phone: "040-432342",
+    street: "Malminkaari 10 A",
+    city: "Helsinki",
+    id: "3d599470-3436-11e9-bc57-8b80ba54c431"
+  },
+  {
+    name: "Venla Ruuska",
+    street: "Nallemäentie 22 C",
+    city: "Helsinki",
+    id: "3d599471-3436-11e9-bc57-8b80ba54c431"
+  }
+];
 
 const typeDefs = `
   type Address {
@@ -71,28 +92,66 @@ const resolvers = {
   Query: {
     personCount: async () => Person.collection.countDocuments(),
     allPersons: async (root, args) => {
-      // filters missing
-      return Person.find({});
+      if (!args.phone) {
+        return Person.find({});
+      }
+
+      return Person.find({ phone: { $exists: args.phone === "YES" } });
     },
     findPerson: async (root, args) => Person.findOne({ name: args.name })
   },
   Person: {
-    address: (root) => {
+    address: ({ street, city }) => {
       return {
-        street: root.street,
-        city: root.city
+        street,
+        city
       };
     }
   },
   Mutation: {
     addPerson: async (root, args) => {
       const person = new Person({ ...args });
-      return person.save();
+      try {
+        await person.save();
+      } catch (error) {
+        throw new GraphQLError("Saving user failed", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.name,
+            error
+          }
+        });
+      }
+
+      return person;
     },
     editNumber: async (root, args) => {
       const person = await Person.findOne({ name: args.name });
       person.phone = args.phone;
-      return person.save();
+      try {
+        await person.save();
+      } catch (error) {
+        throw new GraphQLError("Editing number failed", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.name,
+            error
+          }
+        });
+      }
+
+      return person;
     }
   }
 };
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+startStandaloneServer(server, {
+  listen: { port: 4000 }
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
